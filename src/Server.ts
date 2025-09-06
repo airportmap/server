41b35f69b0cebd5f13edc0fe2bb665d3f9c742d3
komed3 <pk97@outlook.de>
2025-09-06 11:50:40 +0200

    Update Server.ts

--- src/Server.ts
+++ src/Server.ts
@@ -1,46 +1,50 @@
-import type { AppConfig } from '@airportmap/types';
+import type { ServerConfig } from '@airportmap/types';
+import { Debug } from '@server/core/Debug';
 import express, { type Application } from 'express';
 
 export class Server {
 
-    private cfg: AppConfig;
+    private cfg: ServerConfig;
+    private debug: Debug;
+    private modules: string[];
     private app: Application;
 
-    constructor ( cfg: AppConfig ) {
+    constructor ( cfg: ServerConfig ) {
 
         this.cfg = cfg;
+        this.debug = new Debug ( cfg.debug );
         this.app = express();
 
-        this.setup();
+        ( async () => await this.loadModules() )();
 
     }
 
-    private viewEngine () : void {
+    private async loadModules () : Promise< void > {}
 
-        this.app.set( 'view engine', this.cfg._express.viewEngine ?? 'pug' );
-        this.app.set( 'views', this.cfg.paths.views );
+    public get config () : ServerConfig { return this.cfg }
+    public get debugger () : Debug { return this.debug }
+    public get mods () : string[] { return this.modules }
+    public get server () : Application { return this.app }
+
+    public listen () : void {
+
+        const { host, port, https, debug } = this.cfg;
+
+        this.app.listen( port, host, ( err: Error ) => {
+
+            if ( err ) this.debug.err( 'server', `Error starting server`, err );
+            else {
+
+                this.debug.log( 'server', `Airportmap server is running on port: ${ port }`, true );
+                this.debug.log( 'server', `Serving host: ${ host }` );
+                this.debug.log( 'server', `HTTPS enabled: ${ https ? 'yes' : 'no' }` );
+                this.debug.log( 'server', `Debugger enabled: ${ debug ? 'yes' : 'no' }` );
+                this.debug.log( 'server', `Loaded modules: ${ this.modules.join( ', ' ) }` );
+
+            }
+
+        } );
 
     }
-
-    private serveStatic () : void {
-
-        for ( const [ key, path ] of Object.entries( this.cfg.paths.static ) ) {
-
-            this.app.use( `/${ key }`, express.static( path ) );
-
-        }
-
-    }
-
-    public setup () : void {
-
-        this.viewEngine();
-        this.serveStatic();
-
-    }
-
-    public getCfg () : AppConfig { return this.cfg }
-
-    public getApp () : Application { return this.app }
 
 }
