dcd44ed7ae53f0c5a63ebf171fa90706bcf3be74
komed3 <pk97@outlook.de>
2025-09-06 15:05:45 +0200

    i18n setup

--- src/core/SetupI18n.ts
+++ src/core/SetupI18n.ts
@@ -1,5 +1,7 @@
+import type { I18nConfig, I18nLookup } from '@airportmap/types';
 import { type Server } from '@server';
 import { loadJsonConfig, loadYamlConfig } from '@server/core/ConfigLoader';
+import { join } from 'node:path';
 import i18next from 'i18next';
 import FsBackend from 'i18next-fs-backend';
 import { LanguageDetector, handle } from 'i18next-http-middleware';
@@ -8,18 +10,26 @@ export async function setupI18n ( server: Server ) : Promise< boolean > {
 
     if ( server.config.i18n && server.config.i18n.enabled ) {
 
-        const { configPath, lookupPath } = server.config.i18n;
-
-        const config = await loadYamlConfig( configPath );
-        const lookup = await loadJsonConfig( lookupPath );
-
         try {
+
+            const { configPath, lookupPath } = server.config.i18n;
+
+            const config = await loadYamlConfig< I18nConfig >( configPath );
+            const lookup = await loadJsonConfig< I18nLookup >( lookupPath );
 
             await i18next
                 .use( FsBackend )
                 .use( LanguageDetector )
                 .init( {
                     debug: server.debugger.enabled,
+                    cleanCode: true,
+                    fallbackLng: config.fallbackLng,
+                    supportedLngs: lookup.supportedLngs,
+                    preload: config.preload,
+                    ns: lookup.namespaces,
+                    backend: {
+                        loadPath: join( config.path, config.pattern )
+                    },
                     detection: {
                         order: [ 'cookie', 'header' ],
                         lookupCookie: 'locale',
