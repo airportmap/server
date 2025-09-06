621c03fc64b37083669be89067b8f68fd9ea8197
komed3 <pk97@outlook.de>
2025-09-06 14:40:16 +0200

    load config from path

---
+++ src/core/ConfigLoader.ts
@@ -0,0 +1,21 @@
+import { readFile } from 'node:fs/promises';
+import { load } from 'js-yaml';
+
+async function getFileContent ( path: string ) : Promise< string > {
+
+    try { return await readFile( path, { encoding: 'utf8' } ) }
+    catch ( err ) { return '' }
+
+}
+
+export async function loadJsonConfig<T extends object> ( path: string ) : Promise< T > {
+
+    return load( await getFileContent( path ), { json: true } ) as T;
+
+}
+
+export async function loadYamlConfig<T extends object> ( path: string ) : Promise< T > {
+
+    return load( await getFileContent( path ) ) as T;
+
+}
--- src/core/SetupI18n.ts
+++ src/core/SetupI18n.ts
@@ -1,31 +1,46 @@
 import { type Server } from '@server';
+import { loadJsonConfig, loadYamlConfig } from '@server/core/ConfigLoader';
 import i18next from 'i18next';
 import FsBackend from 'i18next-fs-backend';
 import { LanguageDetector, handle } from 'i18next-http-middleware';
 
 export async function setupI18n ( server: Server ) : Promise< boolean > {
 
-    try {
+    if ( server.config.i18n && server.config.i18n.enabled ) {
 
-        await i18next
-            .use( FsBackend )
-            .use( LanguageDetector )
-            .init( {
-                debug: server.debugger.enabled,
-                detection: {
-                    order: [ 'cookie', 'header' ],
-                    lookupCookie: 'locale',
-                    caches: [ 'cookie' ],
-                    cookieSameSite: 'strict',
-                    cookieSecure: server.config.https
-                }
-            } );
+        const { configPath, lookupPath } = server.config.i18n;
 
-        server.app.use( handle( i18next ) );
+        const config = await loadYamlConfig( configPath );
+        const lookup = await loadJsonConfig( lookupPath );
 
-        return true;
+        try {
 
-    } catch ( err ) { server.debugger.err( 'server:i18n', `Error while setting up i18n`, err ) }
+            await i18next
+                .use( FsBackend )
+                .use( LanguageDetector )
+                .init( {
+                    debug: server.debugger.enabled,
+                    detection: {
+                        order: [ 'cookie', 'header' ],
+                        lookupCookie: 'locale',
+                        caches: [ 'cookie' ],
+                        cookieSameSite: 'strict',
+                        cookieSecure: server.config.https
+                    }
+                } );
+
+            server.app.use( handle( i18next ) );
+
+            return true;
+
+        } catch ( err ) {
+
+            server.debugger.err( 'server:i18n', `Error while setting up i18n`, err );
+            process.exit( 1 );
+
+        }
+
+    }
 
     return false;
 
