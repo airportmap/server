2cd1c7be768a60bc85cf0e698b77b4d5eddd44aa
komed3 <pk97@outlook.de>
2025-09-07 00:03:21 +0200

    config loader optional key

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -1,13 +1,14 @@
 import { load } from 'js-yaml';
 import { readFile } from 'node:fs/promises';
 
-export default async function loadConfig < T extends object > ( path: string ) : Promise< T > {
+export default async function loadConfig < T extends object > ( path: string, key?: string ) : Promise< T > {
 
     try {
 
         const content = await readFile( path, { encoding: 'utf8' } );
+        const cfg = load( content, { json: path.endsWith( 'json' ) } ) ?? {} as any;
 
-        return ( load( content, { json: path.endsWith( 'json' ) } ) ?? {} ) as T;
+        return ( key ? key in cfg ? cfg[ key ] : {} : cfg ) as T;
 
     } catch ( err ) { return {} as T }
 
