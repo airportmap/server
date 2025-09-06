ef6b08a4f2948ce2de6c43accf5492b56433f6f3
komed3 <pk97@outlook.de>
2025-09-06 12:25:10 +0200

    server connect/close

--- src/Server.ts
+++ src/Server.ts
@@ -1,3 +1,4 @@
+import { Server as HttpServer } from 'node:http';
 import type { ServerConfig } from '@airportmap/types';
 import { Debug } from '@server/core/Debug';
 import express, { type Application } from 'express';
@@ -7,44 +8,63 @@ export class Server {
     private cfg: ServerConfig;
     private debug: Debug;
     private modules: string[];
-    private app: Application;
+
+    private app?: Application;
+    private server?: HttpServer;
+
+    public get config () : ServerConfig { return this.cfg }
+    public get debugger () : Debug { return this.debug }
+    public get mods () : string[] { return this.modules }
 
     constructor ( cfg: ServerConfig ) {
 
         this.cfg = cfg;
         this.debug = new Debug ( cfg.debug );
-        this.app = express();
-
-        ( async () => await this.loadModules() )();
 
     }
 
     private async loadModules () : Promise< void > {}
 
-    public get config () : ServerConfig { return this.cfg }
-    public get debugger () : Debug { return this.debug }
-    public get mods () : string[] { return this.modules }
-    public get server () : Application { return this.app }
+    private listen () : void {
 
-    public listen () : void {
+        if ( this.app ) this.server = this.app.listen( this.cfg.port, this.cfg.host );
+        else this.debug.err( 'server', `Need to call server.init() first` );
+
+    }
+
+    public async init () : Promise< void > {
+
+        this.app = express();
+
+        await this.loadModules();
+
+    }
+
+    public run () : void {
 
         const { host, port, https, debug } = this.cfg;
 
-        this.app.listen( port, host, ( err: Error ) => {
+        this.listen();
 
-            if ( err ) this.debug.err( 'server', `Error starting server`, err );
-            else {
+        this.server.on( 'connect', () => {
+            this.debug.log( 'server', `Airportmap server is running on port: ${ port }`, true );
+            this.debug.log( 'server', `Serving host: ${ host }` );
+            this.debug.log( 'server', `HTTPS enabled: ${ https ? 'yes' : 'no' }` );
+            this.debug.log( 'server', `Debugger enabled: ${ debug ? 'yes' : 'no' }` );
+            this.debug.log( 'server', `Loaded modules: ${ this.modules.join( ', ' ) }` );
+        } );
 
-                this.debug.log( 'server', `Airportmap server is running on port: ${ port }`, true );
-                this.debug.log( 'server', `Serving host: ${ host }` );
-                this.debug.log( 'server', `HTTPS enabled: ${ https ? 'yes' : 'no' }` );
-                this.debug.log( 'server', `Debugger enabled: ${ debug ? 'yes' : 'no' }` );
-                this.debug.log( 'server', `Loaded modules: ${ this.modules.join( ', ' ) }` );
+        this.server.on( 'close', () => {
+            this.debug.log( 'server', `Airportmap server shut down`, true );
+        } );
 
-            }
-
+        this.server.on( 'error', ( err: Error ) => {
+            this.debug.err( 'server', `Server error occurred`, err );
+            process.exit( 1 );
         } );
 
     }
+
+    public close () : void { if ( this.server ) this.server.close() }
 
 }
