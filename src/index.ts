76e6f372fbcab6605ed7bba98f4350b0ab109601
komed3 <pk97@outlook.de>
2025-09-08 11:51:29 +0200

    add starter function

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,15 @@ import Server from '@server/core/Server';
 import AssetLoader from '@server/helper/AssetLoader';
 import Renderer from '@server/helper/Renderer';
 
-export { Server, AssetLoader, Renderer };
+async function startServer ( path: string, env?: string ) : Promise< Server > {
+
+    const server = new Server( path, env );
+    await server.init();
+
+    server.run();
+
+    return server;
+
+}
+
+export { Server, AssetLoader, Renderer, startServer };
